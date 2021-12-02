e0f7b9e3718d3d677c695493a2ad4b9fa49c4f23
Alessandro Pasqualini <alessandro.pasqualini.1105@gmail.com>
2021-12-02 22:39:30 +0100

    Added support for post_type

--- dartmoon-disable-editor.php
+++ dartmoon-disable-editor.php
@@ -3,7 +3,7 @@
  * Plugin Name:  Dartmoon disable editor 
  * Plugin URI:   https://dartmoon.io
  * Description:  Disable editor for specific templates, pages, posts, etc.
- * Version:      1.0.0
+ * Version:      1.0.1
  * Author:       Dartmoon
  * Author URI:   https://dartmoon.io
  * License:      PROPRIETARY
@@ -16,13 +16,16 @@ function drtn_disable_gutemberg($can_edit, $post_type) {
     // If we are not on the admin interface
     // or there is no ID int the get parameters
     // we canno do anithing
-	if (!is_admin() || !isset($_GET['post'])) {
+	if (!is_admin()) {
         return $can_edit;
     }
 
     // Let the template decide whether 
     // to show the template
-    $can_edit = apply_filters('drtn/disable_gutemberg', $can_edit, (int) $_GET['post']);
+    $post_id = (int) ($_GET['post'] ?? 0);
+    $post_type = $_GET['post_type'] ?? get_post_type($post_id);
+    $can_edit = apply_filters('drtn/disable_gutemberg', $can_edit, $post_id, $post_type);
+
 	return $can_edit;
 }
 add_filter('gutenberg_can_edit_post_type', 'drtn_disable_gutemberg', 10, 2);
@@ -33,19 +36,22 @@ add_filter('use_block_editor_for_post_type', 'drtn_disable_gutemberg', 10, 2);
  *
  */
 function drtn_disable_classic_editor() {
-    // Check if we are showing pages
-	$screen = get_current_screen();
-	if ('page' !== $screen->id || !isset($_GET['post'])) {
+    // If we are not on the admin interface
+    // or there is no ID int the get parameters
+    // we canno do anithing
+	if (!is_admin()) {
         return;
     }
 
     // Let the template decide whether 
     // to show the template
-    $can_edit = apply_filters('drtn/disable_editor', post_type_supports('page', 'editor'), (int) $_GET['post']);
+    $post_id = (int) ($_GET['post'] ?? 0);
+    $post_type = $_GET['post_type'] ?? get_post_type($post_id);
+    $can_edit = apply_filters('drtn/disable_editor', post_type_supports('page', 'editor'), $post_id, $post_type);
     
     // If the user do not want the editor let's remove it
 	if (!$can_edit) {
-		remove_post_type_support('page', 'editor');
+		remove_post_type_support($post_type, 'editor');
 	}
 }
 add_action('admin_head', 'drtn_disable_classic_editor');
